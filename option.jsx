f4f9545dec7b7845be42d56c96afc5c1dd334ff4
Tarif Al Hasan <92237522+tarifalhasan@users.noreply.github.com>
2023-03-19 09:55:18 -0700

    Create option.jsx

---
+++ option.jsx
@@ -0,0 +1,21 @@
+// css code 
+select#Select\ service {
+  outline: none;
+
+  padding: 9px;
+  border-radius: 4px;
+  background: url(../public//image/down-arrow.svg) no-repeat right;
+  -webkit-appearance: none;
+  background-position-x: 99%;
+}
+
+
+// jsx code 
+
+ <select id="Select service" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              >
+                <option selected>Budget</option>
+                <option value="100">100$</option>
+                <option value="200">200$</option>
+                <option value="300">300$</option>
+              </select>
